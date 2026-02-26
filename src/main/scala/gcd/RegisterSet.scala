f27faea2be66db1ea57e1b2497bea9e2a2818782
azat222 <azat222@gmail.com>
2026-02-27 01:21:56 +0300

    add ar register

--- src/main/scala/gcd/RegisterSet.scala
+++ src/main/scala/gcd/RegisterSet.scala
@@ -46,12 +46,17 @@ class RegisterSetFlags extends Bundle {
   val RDIP = Bool()
   val WRSP = Bool()
   val RDSP = Bool()
+  val WRAR = Bool()
+  val LOAD = Bool()
+  val STOR = Bool()
 }
 
 class RegisterSet extends Module {
   val flags  = IO(Input(new RegisterSetFlags()))
   val input = IO(Input(new ResultAttributesBlockOutput()))
   val output = IO(Output(new AluInput()))
+  val memoryInput = IO(Output(new MemoryInput()))
+  val memoryOutput = IO(Input(new MemoryOutput()))
 
   // left side
   private val AC = RegInit(UInt(16.W), 0.U)
@@ -65,12 +70,17 @@ class RegisterSet extends Module {
   private val IP = RegInit(UInt(11.W), 0.U)
   private val SP = RegInit(UInt(11.W), 0.U)
 
+  private val AR = RegInit(UInt(11.W), 0.U)
+
   // todo: memory IO
 
   output.left  := 0.S
   output.right := 0.S
   output.C := PSBits.fromPS(PS).C
   PS := PS | Cat(input.N, input.Z, input.V, input.C) // setting nzvc after operation
+  memoryInput.address := AR
+  memoryInput.write := false.B
+  memoryInput.data := 0.U
 
   when(flags.RDAC)(output.left := AC.asSInt)
   when(flags.RDBR)(output.left := BR.asSInt)
@@ -89,5 +99,15 @@ class RegisterSet extends Module {
   when(flags.WRCR)(CR := input.data.asUInt)
   when(flags.WRIP)(IP := input.data.asUInt)
   when(flags.WRSP)(SP := input.data.asUInt)
+  when(flags.WRAR)(AR := input.data.asUInt)
+
+  // memory connection
+  when(flags.LOAD){
+    DR := memoryOutput.data
+  }
+  when(flags.STOR){
+    memoryInput.data := DR
+    memoryInput.write := true.B
+  }
 
 }
