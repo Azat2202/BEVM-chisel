be6d2b4aada9436a8d0c7cd1a0b1e89e63540f93
azat222 <azat222@gmail.com>
2026-02-27 01:12:44 +0300

    memory made

---
+++ src/main/scala/gcd/Memory.scala
@@ -0,0 +1,24 @@
+package gcd
+
+import chisel3._
+
+class MemoryInput extends Bundle{
+  val write = Bool()
+  val address = UInt(11.W)
+  val data = UInt(16.W)
+}
+
+class MemoryOutput extends Bundle{
+  val data = UInt(16.W)
+}
+
+class Memory extends Module {
+  val input = IO(Input(new MemoryInput()))
+  val output = IO(Output(new MemoryOutput()))
+
+  val mem: SyncReadMem[UInt] = SyncReadMem(2048, UInt(16.W)) // 2048 is size of 7FF memory
+  when(input.write){
+    mem.write(input.address, input.data)
+  }
+  output.data := mem.read(input.address)
+}
